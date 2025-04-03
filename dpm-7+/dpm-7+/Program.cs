4fadc6d729d3dec7332eaabc661baec201a58d95
NKVDream <Kmv235@yandex.ru>
2025-04-03 09:10:27 -0700

    nt

---
+++ dpm-7+/dpm-7+/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Task1
+{
+    static void Main()
+    {
+        string[] words = { "shy", "shock", "shrek" };
+        Task1.Pref(words);
+    }
+    static void Pref(string[] words)
+    {
+
+    }
+}
