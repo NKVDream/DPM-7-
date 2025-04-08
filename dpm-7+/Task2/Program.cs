574b93435d1535a3c40eb7f8bf2295df23751ef5
NKVDream <Kmv235@yandex.ru>
2025-04-07 21:04:22 -0700

    2 done

--- dpm-7+/Task2/Program.cs
+++ dpm-7+/Task2/Program.cs
@@ -3,6 +3,21 @@ class Task2
 {
     static void Main()
     {
-
+        Console.Write("write down anything:");string word = Console.ReadLine();
+        Task2.Change(word);
+    }
+    static void Change(string word)
+    {
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (word[i] == word.ToUpper()[i])
+            {
+                Console.Write(word.ToLower()[i]);
+            }
+            else if (word[i] == word.ToLower()[i])
+            {
+                Console.Write(word.ToUpper()[i]);
+            }
+        }
     }
 }
--- dpm-7+/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ dpm-7+/Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d90de6524e46970298925d90e5f0757d6707ca3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9211184936637c524c6029bcab02c7cf3c227628")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
