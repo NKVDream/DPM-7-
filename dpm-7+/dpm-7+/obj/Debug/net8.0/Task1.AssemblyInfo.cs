5f83cd2965cc532ae284bf605c6fd242e2dead97
NKVDream <Kmv235@yandex.ru>
2025-04-09 19:11:17 -0700

    5-6

--- dpm-7+/dpm-7+/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ dpm-7+/dpm-7+/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8c102979cb21c9c27e3229152c80dd4498b8b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eaaf04190b572d72aebbd583ff95bdc6aa97a6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
